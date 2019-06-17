64f573c27b189620bb5b6ba8d0228a0782d82c20
Manaswini Kundeti <manaswini.kundeti@gmail.com>
2019-06-17 16:20:43 -0700

    Added communication between the 2 activities. MasterListFragment and BodyPartFragment are communicating. Completed Project for Mobile device screen resolution

--- app/src/main/java/com/example/androidmakeoverapp/ui/AndroidMakeoverActivity.java
+++ app/src/main/java/com/example/androidmakeoverapp/ui/AndroidMakeoverActivity.java
@@ -13,7 +13,7 @@ public class AndroidMakeoverActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activty_android_makeover);
 
         //create new fragments only if there are no previously saved fragments
         if(savedInstanceState == null){
@@ -29,19 +29,26 @@ public class AndroidMakeoverActivity extends AppCompatActivity {
             // Set the list of image id's for the head fragment and
             // set the position to the second image in the list
             headFragment.setmImageIds(ImageAssets.getHeads());
-            headFragment.setmListIndex(1);
+            //headFragment.setmListIndex(1);
+            // Retrieve list index values that were sent through an intent; use them to display the desired Android-Me body part image
+            int headIndex = getIntent().getIntExtra("headIndex", 0);
+            headFragment.setmListIndex(headIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.head_fragment_container, headFragment)
                     .commit();
 
             //ADDING BODY FRAGMENT
             bodyFragment.setmImageIds(ImageAssets.getBodies());
+            int bodyIndex = getIntent().getIntExtra("bodyIndex", 0);
+            bodyFragment.setmListIndex(bodyIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.body_fragment_container, bodyFragment)
                     .commit();
 
             //ADDING LEG FRAGMENT
             legFragment.setmImageIds(ImageAssets.getLegs());
+            int legIndex = getIntent().getIntExtra("legIndex", 0);
+            legFragment.setmListIndex(legIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.leg_fragment_container, legFragment)
                     .commit();
--- app/src/main/java/com/example/androidmakeoverapp/ui/MainActivity.java
+++ app/src/main/java/com/example/androidmakeoverapp/ui/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.androidmakeoverapp.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.androidmakeoverapp.R;
 
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
+    //var to store values for list index of the selected images
+    //default value index=0
+    private int headIndex;
+    private int bodyIndex;
+    private int legIndex;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,44 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
     public void onImageSelected(int position) {
         //create toast displaying position
         Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
+
+        // Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
+        // bodyPartNumber will be = 0 for the head fragment, 1 for the body, and 2 for the leg fragment
+        // Dividing by 12 gives us these integer values because each list of images resources has a size of 12
+        int clickedBodyPartNumber = position / 12;
+
+        //store correct list index - always between 0-11
+        int clickedListIndex = position - 12*clickedBodyPartNumber;
+
+        //set the currently displayed item for the correct respective body part fragment
+        switch (clickedBodyPartNumber) {
+            case 0: headIndex = clickedListIndex;
+                break;
+            case 1: bodyIndex = clickedListIndex;
+                break;
+            case 2: legIndex = clickedListIndex;
+                break;
+            default: break;
+        }
+        //put the info in a Bundle and attach it to an Intent that calls AndroidMakeoverActivity
+        Bundle bundle = new Bundle();
+        bundle.putInt("headIndex", headIndex);
+        bundle.putInt("bodyIndex", bodyIndex);
+        bundle.putInt("legIndex", legIndex);
+
+        //attach bundle to intent
+        final Intent intent = new Intent(this, AndroidMakeoverActivity.class);
+        intent.putExtras(bundle);
+
+        //get ref to NextButton and launch intent when this button is clicked
+        Button nextButton = findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
